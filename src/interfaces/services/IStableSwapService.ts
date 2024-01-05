42c75710faae6168cdacddf831d8250c0b1a08bc
Oleksandr Dovgopoliy <alexdovgopoly@gmail.com>
2024-01-05 14:14:41 +0200

    Typo fix

--- src/interfaces/services/IStableSwapService.ts
+++ src/interfaces/services/IStableSwapService.ts
@@ -64,7 +64,7 @@ export default interface IStableSwapService {
 
   getSingleSwapLimitNumerator(): Promise<BigNumber>;
 
-  getSingleSwapLimitDenomerator(): Promise<BigNumber>;
+  getSingleSwapLimitDenominator(): Promise<BigNumber>;
 
   getPoolBalance(tokenAddress: string): Promise<BigNumber>;
 
